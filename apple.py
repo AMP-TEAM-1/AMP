9efb97aff273b0e472d49bba8329f16f66a3a745
ggaa1 <rktqjackd123@naver.com>
2025-09-27 23:03:13 +0900

    apple

---
+++ apple.py
@@ -0,0 +1,22 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.fillcolor("red")
+
+t.begin_fill()
+
+t.circle(100)
+
+t.left(60)
+t.forward(80)
+t.left(120)
+t.forward(80)
+t.left(120)
+t.forward(80)
+
+t.end_fill()
+
+t.hideturtle()
+
+turtle.exitonclick()
